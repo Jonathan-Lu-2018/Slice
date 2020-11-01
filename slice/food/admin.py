839128c7d0d892c54b69fc242c8fc89c4d1a9aac
Jonathan Lu <jonathanlu2582@gmail.com>
2020-11-01 12:48:27 -0800

    created the models and admin

--- slice/food/admin.py
+++ slice/food/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import InNOut, Kfc, LittleCaesars, TacoBell
 
-# Register your models here.
+# InNOut Admin
+class InNOutAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price')
+admin.site.register(InNOut, InNOutAdmin)
+
+class KfcAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price')
+admin.site.register(Kfc, KfcAdmin)
+
+class LittleCaesarsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price')
+admin.site.register(LittleCaesars, LittleCaesarsAdmin)
+
+class TacoBellAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price')
+admin.site.register(TacoBell, TacoBellAdmin)
--- slice/food/models.py
+++ slice/food/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+# InNOut model
+class InNOut(models.Model):
+    name    = models.CharField(max_length=12)
+    price  = models.DecimalField(max_digits=4, decimal_places=2)
+    inNOutImage = models.URLField()
+
+class Kfc(models.Model):
+    name    = models.CharField(max_length=12)
+    price  = models.DecimalField(max_digits=4, decimal_places=2)
+    kfcImage = models.URLField()
+
+class LittleCaesars(models.Model):
+    name    = models.CharField(max_length=12)
+    price  = models.DecimalField(max_digits=4, decimal_places=2)
+    littleCaesarsImage = models.URLField()
+
+class TacoBell(models.Model):
+    name    = models.CharField(max_length=12)
+    price  = models.DecimalField(max_digits=4, decimal_places=2)
+    tacoBellImage = models.URLField()
