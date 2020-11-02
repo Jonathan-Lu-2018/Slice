7623bc3e6ceb668c29999f93031a15ff12d41b7e
Jonathan Lu <jonathanlu2582@gmail.com>
2020-11-01 16:26:17 -0800

    changed the character length

--- slice/food/models.py
+++ slice/food/models.py
@@ -2,21 +2,21 @@ from django.db import models
 
 # InNOut model
 class InNOut(models.Model):
-    name    = models.CharField(max_length=12)
+    name    = models.CharField(max_length=50)
     price  = models.DecimalField(max_digits=4, decimal_places=2)
     inNOutImage = models.URLField()
 
 class Kfc(models.Model):
-    name    = models.CharField(max_length=12)
+    name    = models.CharField(max_length=50)
     price  = models.DecimalField(max_digits=4, decimal_places=2)
     kfcImage = models.URLField()
 
 class LittleCaesars(models.Model):
-    name    = models.CharField(max_length=12)
+    name    = models.CharField(max_length=50)
     price  = models.DecimalField(max_digits=4, decimal_places=2)
     littleCaesarsImage = models.URLField()
 
 class TacoBell(models.Model):
-    name    = models.CharField(max_length=12)
+    name    = models.CharField(max_length=50)
     price  = models.DecimalField(max_digits=4, decimal_places=2)
     tacoBellImage = models.URLField()
