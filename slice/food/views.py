3cf8bfcb3c2743ec07730a14de7f42da0f36c062
Jonathan Lu <jonathanlu2582@gmail.com>
2020-11-03 10:31:42 -0800

    added the food menu

--- slice/food/views.py
+++ slice/food/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import InNOut, Kfc, LittleCaesars, TacoBell
 # Create your views here.
 
 # Home Page View
@@ -7,13 +8,21 @@ def index(request):
     return render(request, 'food/index.html')
 
 def inAndOut(request):
-    return render(request, 'food/inAndOut.html')
+    inAndOuts = InNOut.objects.all()
+    ctx = {'inAndOuts':inAndOuts}
+    return render(request, 'food/inAndOut.html', ctx)
 
 def littleCaesars(request):
-    return render(request, 'food/littleCaesars.html')
+    littleCaesarss = LittleCaesars.objects.all()
+    ctx = {'littleCaesarss':littleCaesarss}
+    return render(request, 'food/littleCaesars.html', ctx)
 
 def kfc(request):
-    return render(request, 'food/kfc.html')
+    kfcs = Kfc.objects.all()
+    ctx = {'kfcs': kfcs}
+    return render(request, 'food/kfc.html', ctx)
 
 def tacoBell(request):
-    return render(request, 'food/tacoBell.html')
+    tacoBells = TacoBell.objects.all()
+    ctx = {'tacoBells': tacoBells}
+    return render(request, 'food/tacoBell.html', ctx)
