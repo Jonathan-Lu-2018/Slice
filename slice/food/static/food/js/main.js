f5199337f0bf95f1855cdfad2df71809639d8724
Jonathan Lu <jonathanlu2582@gmail.com>
2020-11-03 14:51:05 -0800

    set up the shopping cart

--- slice/food/static/food/js/main.js
+++ slice/food/static/food/js/main.js
@@ -1,1 +1,14 @@
-document.querySelector('#test').innerHTML = "<h1> Slice </h1>";
+var hours = 24;
+var now = new Date.getTime();
+var stepTime = localStorage.getItem('stepTime');
+
+// Checks if the user has accessed the app within 24 hrs
+if(stepTime == null){
+    localStorage.setItem('stepTime', now);
+}
+else{
+    if(now - stepTime > hours*60*60*1000){
+        localStorage.clear();
+        localStorage.setItem('stepTime', now);
+    }
+}
