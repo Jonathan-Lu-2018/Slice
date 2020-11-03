e00506b9816545f660d199e4c9dba0420cf37a28
Jonathan Lu <jonathanlu2582@gmail.com>
2020-11-03 10:59:15 -0800

    added active links

--- slice/food/views.py
+++ slice/food/views.py
@@ -5,24 +5,25 @@ from .models import InNOut, Kfc, LittleCaesars, TacoBell
 
 # Home Page View
 def index(request):
-    return render(request, 'food/index.html')
+    ctx = {'active_link': 'index'}
+    return render(request, 'food/index.html', ctx)
 
 def inAndOut(request):
     inAndOuts = InNOut.objects.all()
-    ctx = {'inAndOuts':inAndOuts}
+    ctx = {'inAndOuts':inAndOuts, 'active_link': 'inAndOut'}
     return render(request, 'food/inAndOut.html', ctx)
 
 def littleCaesars(request):
     littleCaesarss = LittleCaesars.objects.all()
-    ctx = {'littleCaesarss':littleCaesarss}
+    ctx = {'littleCaesarss':littleCaesarss, 'active_link': 'littleCaesars'}
     return render(request, 'food/littleCaesars.html', ctx)
 
 def kfc(request):
     kfcs = Kfc.objects.all()
-    ctx = {'kfcs': kfcs}
+    ctx = {'kfcs': kfcs, 'active_link': 'kfc'}
     return render(request, 'food/kfc.html', ctx)
 
 def tacoBell(request):
     tacoBells = TacoBell.objects.all()
-    ctx = {'tacoBells': tacoBells}
+    ctx = {'tacoBells': tacoBells, 'active_link': 'tacoBell'}
     return render(request, 'food/tacoBell.html', ctx)
